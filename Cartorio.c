cfab2db0bc300a44a87f1a620a936f11b6ca4a55
DanielBerga-dev <berga-daniel@outlook.com>
2024-11-22 17:08:44 -0300

    Adição de sistema de Login

--- Cartorio.c
+++ Cartorio.c
@@ -3,6 +3,14 @@
 #include <locale.h> //biblioteca de alocacoes de texto por regiao
 #include <string.h> //bibilioteca resposavel por cuidas das string
 
+typedef struct{
+    char login[30]; // vetor login da struct pessoa
+    char senha[30]; // vetor senha da struct pessoa
+} pessoa; pessoa p[1];  // diminuindo o nome da struct para "p" e o "[1]" é o máximo de pessoas com logins e senhas
+
+
+
+
 int Registro()		// funcao responsavel por cadastrar os usuarios no sistema 
 {
 	//inicio criacao de variaveis/string
@@ -117,6 +125,28 @@ int Deletar()
 
 int main ()
 {
+	char login[30]; // responsável por armazenar a senha inserida pelo usuário
+    char senha[30]; // responsável por armazenar a senha inserida pelo usuário
+
+    strcpy(p[0].login, "admin"); // Definindo o login "admin" na struct
+    strcpy(p[0].senha, "123"); // Definindo a senha "123"  na struct
+
+    printf("\nlogin:");
+    scanf("%s", login); // armazenando os dados inseridos pelo usuário para o vetor login que está dentro da função main
+
+    printf("\nsenha:");
+    scanf("%s", senha); // armazenando os dados inseridos pelo usuário para o vetor senha que está dentro da função main
+
+    if ((strcmp(login,p[0].login)==0) && (strcmp(senha,p[0].senha)==0)){ // A função strcmp é responsável por comparar string com string
+        printf("Usuário logado"); // se os vetores de dentro da struct tiver os mesmos dados do vetor interno da função main, usuário será logado.
+    }else{
+        printf("Login e/ou senha incorretos"); // senão, login ou senha incorretos.
+    }
+    
+	
+	
+	
+	
     int opcao=0; //definindo as variaveis
     int x=1;
     for(x=1;x=1;)
